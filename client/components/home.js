e3e72bd5b09a71a4b072889e7d26c5ff24248cca
kaplenko-sa72 <suziriya.kaplenko05@gmail.com>
2019-11-28 20:30:41 +0200

    localStorage

--- client/components/home.js
+++ client/components/home.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {} from 'universal-cookie';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
 import Head from './head'
 
 const Home = () => {
-  const [counter] = useState(0)
+  let initialCounter = 0
+  if (typeof localStorage !== 'undefined'
+    && typeof localStorage.getItem('counter') !== 'undefined') {
+    initialCounter = +localStorage.getItem('counter')
+  }
+  const [counter, setCounterNew] = useState(initialCounter)
+  const updateCounter = () => {
+    setCounterNew(counter)
+    localStorage.setItem('counter', counter)
+  }
   return (
     <div>
-      <Link to="/">&lArr;</Link>
+      <button type="button" onClick={updateCounter}>Update Counter</button>
+      <button type="button"><Link to="/">{counter} + &lArr;</Link></button>
       <Head title="Hello" />
       <div> Hello World {counter} </div>
     </div>
